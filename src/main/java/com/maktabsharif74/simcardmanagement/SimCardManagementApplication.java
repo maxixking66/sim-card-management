ee08618d0cd8fafa4d20215d3d82d77a7e59e38a
maxixking66 <maxixking66@gamil.com>
2022-06-23 11:33:20 +0430

    init main

--- src/main/java/com/maktabsharif74/simcardmanagement/SimCardManagementApplication.java
+++ src/main/java/com/maktabsharif74/simcardmanagement/SimCardManagementApplication.java
@@ -1,8 +1,15 @@
 package com.maktabsharif74.simcardmanagement;
 
+import com.maktabsharif74.simcardmanagement.util.HibernateUtil;
+
+import javax.persistence.EntityManager;
+
 public class SimCardManagementApplication {
 
     public static void main(String[] args) {
+
+        EntityManager entityManager =
+                HibernateUtil.getEntityManagerFactory().createEntityManager();
 
     }
 }
